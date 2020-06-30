2d7aca66881000cb861de39cfdfcdfce794b5220
flinthenderson <flint.alex.henderson@gmail.com>
2020-06-30 23:01:46 +0300

    basic funcs

---
+++ friendster/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FriendsterConfig(AppConfig):
+    name = 'friendster'
---
+++ friendster/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import requests
+from social_django.models import UserSocialAuth
+# Create your views here.
+def index(request):
+	context = {}
+	return render(request, 'friendster/index.html', context)
+
+def friends(request):
+	user = request.user
+	authuser = UserSocialAuth.objects.get(user_id=user.id)
+	getRecent = 'https://api.vk.com/method/friends.getRecent?'
+	payload = {'uid': str(authuser.extra_data['id']),
+			   'access_token': authuser.extra_data['access_token'],
+			   'v': '5.120'}
+
+	r = requests.get(getRecent, params=payload)
+	listOfFriendsIds = r.json()['response']
+	context = {'friends': listOfFriendsIds}
+	return render(request, 'friendster/friends.html', context)
+
+def get_list_of_friends(request, listOfFriendsIds, access_token):
+	getProfile = 'https://api.vk.com/method/users.get?'
+	listOfFriendsNames = []
+	for friendId in listOfFriendsIds:
+		payload = {'user_id': friendId, 'access_token': access_token, 'v': '5.120'}
+		r = requests.get(getProfile, params=payload)
+		first_name = r.json()['response'][0]['first_name']
+		last_name = r.json()['response'][0]['last_name']
+		listOfFriendsNames.append({str(friendId): [first_name, last_name]})
+
+	return listOfFriendsNames
